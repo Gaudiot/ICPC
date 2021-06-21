5d8a0b58712f155a59ea5c68c4ce556f85d195fb
victor <vefg@cin.ufpe.br>
2021-06-21 15:17:29 -0300

    basic script && rabin with struct

---
+++ Biblioteca/String/Rabin.cpp
@@ -0,0 +1,27 @@
+struct Rabin{
+    ll base, mod, sz;
+    vll pot, has;
+
+    Rabin(string &str, ll mod = 997 , ll base = 1e9+7): mod(mod), base(base){
+        this->sz = str.size();
+        pot.resize(this->sz+1);
+        has.resize(this->sz+1);
+
+        build(str);
+    }
+
+    void build(string &str){
+        pot[0] = 1;
+        has[0] = str[0];
+        for(int i = 1 ; i < sz ; i++){
+            pot[i] = (pot[i-1]*base)%mod;
+            has[i] = ((has[i-1]*base) + str[i])%mod;
+        }
+    }
+
+    ll getkey(ll l, ll r){
+        ll ans = has[r];
+        if(l > 0) ans = ((ans - pot[r-l+1]*has[l-1])%mod + mod)%mod;
+        return ans;
+    }
+};
---
+++ Biblioteca/basic.cpp
@@ -0,0 +1,19 @@
+// to compile run this command
+// g++ file.cpp -o a -std=c++17 -Wfatal-errors
+// to run the file
+// ./a < in
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+#define endl '\n'
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    return 0;
+}
