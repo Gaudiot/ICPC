502b6e6dc854691974e9eec9f20be2f4d46143db
Gaudiot <victor.gaudiot@gmail.com>
2021-09-29 20:37:26 -0300

    update DSU to a struct

--- Biblioteca/Graph/Dsu.cpp
+++ Biblioteca/Graph/Dsu.cpp
@@ -1,21 +1,35 @@
-vll ds(n);
-vll sz(n);
+struct Dsu{
+    vll ds;
+    vll sz;
 
-void dsBuild(){
-    for(int i = 0 ; i < n ; i++){
-        ds[i] = i;
-        sz[i] = 1;
+    Dsu(ll n){
+        ds.resize(n);
+        sz.resize(n);
+
+        build(n);
     }
-}
 
-ll dsFind(ll i){
-    if(ds[i] != i) ds[i] = dsFind(ds[i]);
-    return ds[i];
-}
+    void build(ll n){
+        for(int i = 0 ; i < n ; i++){
+            ds[i] = i;
+            sz[i] = 1;
+        }
+    }
 
-void dsUnion(ll a, ll b){
-    a = dsFind(a); b = dsFind(b);
-    if(sz[a] < sz[b]) swap(a, b);
-    if(a != b) sz[a] += sz[b];
-    ds[b] = a;
-}
+    ll find(ll i){
+        if(ds[i] != i) ds[i] = find(ds[i]);
+        return ds[i];
+    }
+
+    void merge(ll u, ll v){
+        u = find(u); v = find(v);
+        if(sz[u] < sz[v]) swap(u, v);
+        if(u != v) sz[u] += sz[v];
+        ds[v] = u;
+    }
+
+    ll getSize(ll i){
+        i = find(i);
+        return sz[i];
+    }
+};
