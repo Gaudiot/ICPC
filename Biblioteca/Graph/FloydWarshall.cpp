bee78464270a11970eea7710d03a4604375ff6bc
Gaudiot <victor.gaudiot@gmail.com>
2022-02-01 13:01:20 -0300

    Floyd now works properly

--- Biblioteca/Graph/FloydWarshall.cpp
+++ Biblioteca/Graph/FloydWarshall.cpp
@@ -3,9 +3,9 @@ ll n; //number of nodes
 //-1 distance means there is no road connecting the points
 vector<vll> dist(n, vll(n, -1));
 
-for(int i = 0 ; i < n ; i++){
-    for(int j = 0 ; j < n ; j++){
-        for(int k = 0 ; k < n ; k++){
+for(int k = 0 ; k < n ; k++){
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < n ; j++){
             if(dist[i][k] == -1LL || dist[k][j] == -1LL) continue;
             if(dist[i][j] == -1 || dist[i][j] > dist[i][k] + dist[k][j]){
                 dist[i][j] = dist[i][k] + dist[k][j];
