6e13edbba9bf8313ebbe9683f5e2210ee27b6242
victor <vefg@cin.ufpe.br>
2022-06-24 06:37:42 -0300

    LCA set first parent

--- Biblioteca/Graph/Lca.cpp
+++ Biblioteca/Graph/Lca.cpp
@@ -6,20 +6,21 @@ vector<vector<ll>> up(n, vector<ll>(logn, -1));
 
 void dfs(ll node = 0){ //Starts at root
     for(auto next: adj[node]){
+        up[next][0] = node;
         depth[next] = depth[node] + 1;
         dfs(next);
     }
 }
 
 void build(){
+    dfs();
+
     for(int j = 1 ; j < logn ; j++){
         for(int i = 0 ; i < n ; i++){
             if(up[i][j-1] == -1) continue;
             up[i][j] = up[up[i][j-1]][j-1];
         }
     }
-
-    dfs();
 }
 
 ll getLca(ll a, ll b){
