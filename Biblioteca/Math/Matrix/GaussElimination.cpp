bcb96e191e41446a278414df9e204e216bd23ecd
Gaudiot <victor.gaudiot@gmail.com>
2022-09-09 00:28:58 -0300

    update gauss elimination

--- Biblioteca/Math/Matrix/GaussElimination.cpp
+++ Biblioteca/Math/Matrix/GaussElimination.cpp
@@ -12,24 +12,32 @@ struct Matrix{
     }
 
     void gauss(){
-        for(int j = 0 ; j < min(n, m) ; j++){
-            ll pivot = j;
+        ll p = 0;
+        for(int k = 0 ; k < n ; k++){
+            ll pivot = p;
             //look for pivot
-            for(int i = j ; i < n ; i++){
-                if(abs(mat[i][j]) > abs(mat[pivot][j])){
+            for(int i = p ; i < n ; i++){
+                if(abs(mat[i][k]) > abs(mat[pivot][k])){
                     pivot = i;
                 }
             }
             //pivot has null value
-            if(abs(mat[pivot][j]) < eps) continue;
-            for(int i = 0 ; i < m ; i++) swap(mat[j][i], mat[pivot][i]);
+            if(abs(mat[pivot][k]) < eps) assert(0);
+            for(int i = 0 ; i < m ; i++) swap(mat[pivot][i], mat[p][i]);
+
+            ld g = mat[p][k];
+            for(int i = 0 ; i < m ; i++){
+                mat[p][i] /= g;
+            }
 
             for(int i = 0 ; i < n ; i++){
-                if(i != j){
-                    ld c = mat[i][j]/mat[j][j];
-                    for(int k = 0 ; k < m ; k++) mat[i][k] -= c*mat[j][k];
+                if(i != p){
+                    ld c = mat[i][k]/mat[p][k];
+                    for(int j = 0 ; j < m ; j++) mat[i][j] -= c*mat[p][j];
                 }
             }
+
+            p++;
         }
     }
 };
